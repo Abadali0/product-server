a654cb9495d3b5cbfc805c499951efe896f5ae3b
Abadali0 <abadop53@gmail.com>
2026-09-23 14:35:58 +0500

    new lec

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 import express from "express";
-const app=express()
+const app = express();
+ app.use(express.json());
+let products = [
+  {
+    id: 1,
+    name: "Corsair HS45 Headphone",
+    price: 4500,
+    imageUrl: "https://techmatched.pk/wp-content/uploads/2024/05/4-13.png",
+    desc: "A comfortable and high-quality gaming.",
+  },
+  {
+    id: 2,
+    name: "RTX 3060",
+    price: 93000,
+    imageUrl:
+      "https://static.wbx.pk/files/2603/Images/14-czone.com.pk-1540-12831-250122082031-2603-2261410-231124021614482.jpg",
+    desc: "A powerful graphics card from nvidia.",
+  },
+];
 
-        //Get API Create 
+app.get("/products", (req, res)=>{
+    res.json(products);
 
-app.get("/working",(req,res)=>{
-        res.send("I am working Perfectly")
-})
-app.get("/profile",(req,res)=>{
-        res.status(200).json({
-            name:"Abad Ali",
-            userId:"202438"
-            
-        })
-})
-const PORT=5050
+});
 
-app.listen(PORT,()=>{
-    console.log("server is Running on PORT 5050")
+app.post("/products", (req, res) => {
+  const newProduct = req.body;
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+});
+
+
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  products = products.filter((product) => product.id !== parseInt(id));
+  res.status(204).send();
+});
+
+app.listen(5050, ()=>{
+    console.log("Server running on port 5050")
 })
